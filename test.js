7f85cdc5e6b41e7fe2285c03a4c2332255b327db
ProjectAI <rahuldas455@outlook.com>
2023-10-26 16:51:52 +0530

    car parking softweare

--- test.js
+++ test.js
@@ -1,104 +1,87 @@
-// test.js file
+document.getElementById('parking-data-entry').addEventListener("click", function() {
+	document.querySelector('.parking').style.display = "flex";
+});
 
-function updateWT() {
-    var num1 = parseInt(document.getElementById("num1").value);
-    var num2 = parseInt(document.getElementById("num2").value);
-    var result = num1 - num2;
-    
-    if (result < 0) {
-      result = Math.abs(result);}
-    
-    document.getElementById("result").value = result;
-  }
-  
-  // time function
-  function updateTime()
-  {
-      var currentTime = new Date();
-      var hours = currentTime.getHours();
-      var minutes = currentTime.getMinutes();
-      var seconds = currentTime.getSeconds();
-     
-      if (hours < 10) {
-          hours = "0" + hours;
-      } 
-  
-      if (minutes < 10) {
-        minutes = "0" + minutes;
-      } 
-      
-       if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-      
-      var timeString = hours + ":" + minutes +":" + seconds ;
-      
-      document.getElementById("time").value = timeString;
-      document.getElementById("time2").value = timeString;
-  }
-    
-    setInterval(updateTime, 1000);
-    
-  
-  
-    // date function
-  
-  function getDate()
-  {
-      const today= new Date();
-  
-      const newdate = today.toISOString().substring(0,10);
-  
-      document.getElementById("date").value = newdate;
-      document.getElementById("date2").value = newdate;
-  }
-  setInterval(getDate, 1000);
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  //  serial id generator make it more senseable
-  
-  // <html>
-  // <head>
-  // <script>
-  // function generateID() {
-  //   // Define the possible characters for the ID
-  //   var chars = "ABCDEFGHJKLMNPQRSTUVWXYZ123456789";
-  //   // Initialize an empty string for the ID
-  //   var id = "";
-  //   // Loop three times to generate three characters
-  //   for (var i = 0; i < 3; i++) {
-  //     // Pick a random index from the chars string
-  //     var index = Math.floor(Math.random() * chars.length);
-  //     // Append the character at that index to the ID
-  //     id += chars[index];
-  //   }
-  //   // Check if the ID contains at least one number and one uppercase letter
-  //   var hasNumber = /\d/.test(id);
-  //   var hasLetter = /[A-Z]/.test(id);
-  //   // If not, recursively call the function again until it does
-  //   if (!hasNumber || !hasLetter) {
-  //     id = generateID();
-  //   }
-  //   // Return the ID
-  //   return id;
-  // }
-  // </script>
-  // </head>
-  // <body>
-  // <h1>Generate a 3-letter alphanumeric ID</h1>
-  // <p id="demo"></p>
-  // <button onclick="document.getElementById('demo').innerHTML = generateID();">Click me</button>
-  // </body>
-  // </html>
-  
+document.querySelector('.close-parking').addEventListener("click", function() {
+	document.querySelector('.parking').style.display = "none";
+});
+
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["parking_operator"] = document.getElementById("parking_operator").value;
+    formData["parking_vehical-no"] = document.getElementById("parking_vehical-no").value;
+    formData["parking_time"] = document.getElementById("parking_time").value;
+    formData["parking_charge"] = document.getElementById("parking_charge").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("parking-data-entry").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(1);
+    cell1.innerHTML = data.parking_operator;
+    cell2 = newRow.insertCell(2);
+    cell2.innerHTML = data.parking_vehical-no;
+    cell3 = newRow.insertCell(3);
+    cell3.innerHTML = data.parking_time;
+    cell4 = newRow.insertCell(5);
+    cell4.innerHTML = data.parking_charge;
+    cell4 = newRow.insertCell(6);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("parking_operator").value = "";
+    document.getElementById("parking_vehical-no").value = "";
+    document.getElementById("parking_time").value = "";
+    document.getElementById("parking_charge").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("parking_operator").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("parking_vehical-no").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("parking_time").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("parking_charge").value = selectedRow.cells[5].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[1].innerHTML = formData.parking_operator;
+    selectedRow.cells[2].innerHTML = formData.parking_vehical-no;
+    selectedRow.cells[3].innerHTML = formData.parking_time;
+    selectedRow.cells[5].innerHTML = formData.parking_charge;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("parking_operator").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
